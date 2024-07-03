b4ee562b888419187d041de11a9e6a6de1569e23
Takeo <urkn99343@me.pikara.ne.jp>
2024-07-04 01:37:55 +0900

    コミット

---
+++ app/Models/Bunbougu.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Bunbougu extends Model
+{
+    use HasFactory;
+
+    //userとのリレーション関係
+    public function user(){
+        
+        return $this->belongsTo(User::class);
+    }
+}
